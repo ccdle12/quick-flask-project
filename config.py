a3cf40fbc3efd589a626b66751ab25dfdbdad058
ccdle12 <chris.coverdale24@gmail.com>
2019-06-29 08:31:08 +0100

    config.py: adding env variable

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 class Config(object):
+    ENV = 'production'
     DEBUG = False
     TESTING = False
 
 class DevelopmentConfig(Config):
+    ENV = 'development'
     DEBUG = True
-    TESTING = True
