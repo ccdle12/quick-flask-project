ea72ff54af80f6863fac29a4b1c86464b429fad5
ccdle12 <chris.coverdale24@gmail.com>
2019-06-29 09:52:58 +0100

    simple_app/cmc_service.py: adding cmc_service for requests to coin market

---
+++ simple_app/cmc_service.py
@@ -0,0 +1,34 @@
+from simple_app import app
+from requests import Request, Session
+import json
+
+class CMCService():
+    """ Constant Private variables used when providing different requests """
+    _BASE_URL = 'https://pro-api.coinmarketcap.com/v1'
+    _QUOTES = '/cryptocurrency/quotes/latest'
+
+    """ Default headers for sending requests to coin market cap """
+    _headers = {
+      'Accepts': 'application/json',
+      'X-CMC_PRO_API_KEY': app.config['CMC_SECRET_KEY']
+    }
+
+    """ Session for each request """
+    _session = None
+
+    def __init__(self):
+      self._session = Session()
+      self._session.headers.update(self._headers)
+      
+    def get_quote(self, ticker):
+       """ Get a quote for a given ticket symbol """
+       url = '{}{}'.format(self._BASE_URL, self._QUOTES)
+       parameters = {
+         'id': ticker
+       }
+
+       response = self._session.get(url, params=parameters)
+
+       return json.loads(response.text)
+
+
