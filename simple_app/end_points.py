4184fbbb503c05378c8ebe06144a31bde3b2d906
ccdle12 <chris.coverdale24@gmail.com>
2019-06-29 10:43:41 +0100

    simple_app/end_points.py: adding map

--- simple_app/end_points.py
+++ simple_app/end_points.py
@@ -2,6 +2,13 @@ from flask import jsonify
 from simple_app import app
 from simple_app import cmc_service
 
+@app.route('/map')
+def map():
+  coin_market_service = cmc_service.CMCService()
+  data = coin_market_service.map()
+
+  return jsonify(data)
+
 @app.route('/')
 def index():
     """ A simple function that returns hello: world """
