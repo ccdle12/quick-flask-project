ccdbe2ea106faa9a02ca662ab64240afa7aa56e9
ccdle12 <chris.coverdale24@gmail.com>
2019-06-29 19:32:16 +0100

    simple_app/routes/cmc_routes.py: before first request function

--- simple_app/routes/cmc_routes.py
+++ simple_app/routes/cmc_routes.py
@@ -1,27 +1,40 @@
-from simple_app import app
+from simple_app import app, db
 from simple_app.services import cmc_service
 from simple_app.decorators import token_required
-from flask import jsonify
+from simple_app.models import CoinID
+from flask import jsonify, request
 
 @app.before_first_request
-def activate_job():
+def fetch_coin_ids():
+    """Function that runs before the first request. It fetches all the ids 
+       from the coin market cap API and writes it to the db along with the
+       corresponding symbol.
+    """
+    print('Caching the coin ids...')
     coin_market_service = cmc_service.CMCService()
     data = coin_market_service.map()
+    
+    coins = []
+    for d in data['data']:
+        exists = bool(CoinID.query.filter_by(id=int(d['id'])).first())
+        if not exists:
+            coins.append(CoinID(id=int(d['id'], symbol=d['symbol'])))
 
-    print(data)
+    db.session.add_all(coins)
+    db.session.commit()
 
-@app.route('/map')
-def map():
-    coin_market_service = cmc_service.CMCService()
-    data = coin_market_service.map()
-
-    return jsonify(data)
-
-@app.route('/')
+@app.route('/quote', methods=['GET'])
 @token_required
-def index(current_user):
-    """ A simple function that returns hello: world """
+def get_quote(current_user):
+    """Request to retrieve a quote given a symbol. The symbol is passed as a 
+        query string in the url e.g. '/quote?symbol=btc'.
+    """
+    symbol = request.args['symbol']
+    coin = CoinID.query.filter_by(symbol=symbol.upper()).first()
+    if not coin:
+        return jsonify({'message': 'coin does not exist'}), 404
+
     coin_market_service = cmc_service.CMCService()
-    data = coin_market_service.get_quote(1)
+    data = coin_market_service.get_quote(coin.id)
 
     return jsonify(data)
