7e6cb8125f6123fe9081d39d623b5de931c70ddc
ccdle12 <chris.coverdale24@gmail.com>
2019-06-29 09:52:39 +0100

    simple_app/end_points.py: moving requests to cmc_service

--- simple_app/end_points.py
+++ simple_app/end_points.py
@@ -1,7 +1,11 @@
 from flask import jsonify
 from simple_app import app
+from simple_app import cmc_service
 
 @app.route('/')
-def index() -> str:
-    """A simple function that returns hello: world"""
-    return jsonify('{hello: world}')
+def index():
+    """ A simple function that returns hello: world """
+    coin_market_service = cmc_service.CMCService()
+    data = coin_market_service.get_quote(1)
+
+    return jsonify(data)
