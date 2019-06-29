e5aefbe68c59165d5a64e3bb926442080f92d69d
ccdle12 <chris.coverdale24@gmail.com>
2019-06-29 16:10:45 +0100

    __init__.py: updating package includes

--- simple_app/__init__.py
+++ simple_app/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
-import os
+from flask_sqlalchemy import SQLAlchemy
 
 """ Loads the secret variables from the instance folder """
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
-from simple_app import end_points
+""" Connects to an instance of SQLALCHEMY """
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+from simple_app import models
+from simple_app import decorators
+from simple_app.routes import user_routes, cmc_routes
+from simple_app.services import cmc_service
